2baf71f802eb9fd1360503de9ea990fccc438681
Ferran Garriga Olle <zegnus@gmail.com>
2020-12-18 15:52:23 +0000

    fix today's birthday

--- screens/birthday-list/birthday-list-business-logic/src/commonMain/kotlin/com/zegnus/birthdaylist/businesslogic/presentation/BirthdayViewModel.kt
+++ screens/birthday-list/birthday-list-business-logic/src/commonMain/kotlin/com/zegnus/birthdaylist/businesslogic/presentation/BirthdayViewModel.kt
@@ -1,20 +1,38 @@
 package com.zegnus.birthdaylist.businesslogic.presentation
 
+import com.soywiz.klock.DateTime
+import com.soywiz.klock.TimeSpan
+import com.soywiz.klock.Year
 import com.zegnus.birthdaylist.businesslogic.domain.Birthday
 import com.zegnus.birthdaylist.businesslogic.domain.Sex
 
 data class BirthdayViewModel(val text: String)
 
 internal fun Birthday.toViewModel(time: Time): BirthdayViewModel {
-//    val remainingMonths = remainingMonths(time)
-//    val whenIsTheBirthdayType = whenIsTheBirthday(date, time.now())
-//    val whenIsTheBirthdayText = "whenIsTheBirthdayType.toText()"
-//
-//    val text = "$name's birthday is $whenIsTheBirthdayText, " +
-//            "on the ${date.dayOfMonth}${date.dayOfMonth.toOrdinal()} of ${date.month} " +
-//            "${personReference()} will be ${yearsOldNumber(time)} years old"
-//    return BirthdayViewModel(text)
-    return BirthdayViewModel("text")
+    val whenIsTheBirthdayType = whenIsTheBirthdayType(date, time.now())
+    val yearsOld: Int = time.now().year - date.year + 1
+
+    val text = when (whenIsTheBirthdayType) {
+        WhenIsTheBirthdayType.Today -> "$name's birthday is today, ${personReference()} is $yearsOld years old"
+        WhenIsTheBirthdayType.Tomorrow -> "TODO()"
+        WhenIsTheBirthdayType.NextDay -> "TODO()"
+        WhenIsTheBirthdayType.ThisMonth -> "TODO()"
+        WhenIsTheBirthdayType.NextMonth -> "TODO()"
+        WhenIsTheBirthdayType.MoreThanAMonth -> "TODO()"
+    }
+    return BirthdayViewModel(text)
+}
+
+private fun whenIsTheBirthdayType(birthday: DateTime, now: DateTime): WhenIsTheBirthdayType {
+    val timeDifference = timeDifference(now, birthday)
+    return when {
+        timeDifference.days.toInt() == 0 -> WhenIsTheBirthdayType.Today
+        timeDifference.days.toInt() == 1 -> WhenIsTheBirthdayType.Tomorrow
+        timeDifference.days.toInt() <= 7 -> WhenIsTheBirthdayType.NextDay
+        birthday.month0 - now.month0 == 0 -> WhenIsTheBirthdayType.ThisMonth
+        birthday.month0 - now.month0 == 1 -> WhenIsTheBirthdayType.NextMonth
+        else -> WhenIsTheBirthdayType.MoreThanAMonth
+    }
 }
 
 private fun Birthday.personReference(): String =
@@ -25,14 +43,12 @@ private fun Birthday.personReference(): String =
     }
 
 private enum class WhenIsTheBirthdayType {
-    Today,
-    Tomorrow,
-    ThisWeek,
-    NextWeek,
-    InTwoWeeks,
-    ThisMonth,
-    NextMonth,
-    InMoreThanAMonth
+    Today, // Anna's birthday is today, she will is 34 years old
+    Tomorrow, // Anna's birthday is tomorrow, she will be 34 years old
+    NextDay, // Anna's birthday is next Tuesday on the 24th of January, she will is 34 years old
+    ThisMonth, // Anna's birthday is this month on the 24th of January, she will is 34 years old
+    NextMonth, // Anna's birthday is next month on the 24th of January, she will is 34 years old
+    MoreThanAMonth, // Anna's birthday is in 2 months on the 24th of Jaunary, she will is 34 years old
 }
 
 private fun Int.toOrdinal(): String {
@@ -47,3 +63,22 @@ private fun Int.toOrdinal(): String {
         else -> "th"
     }
 }
+
+private fun timeDifference(now: DateTime, birthday: DateTime): TimeSpan {
+    val birthdayThisYear = birthday.copyDayOfMonth(year = now.year)
+
+    val diff = birthdayThisYear.minus(now)
+
+    return if (diff.days < 0) {
+        val nextYear = birthdayThisYear.copyDayOfMonth(year = now.year + 1)
+        nextYear.minus(now)
+    } else {
+        diff
+    }
+}
+
+//            BirthdayViewModel("Anna's birthday is today, she will is 34 years old"),
+//            //BirthdayViewModel("Anna's birthday is in 4 months, on the 13th of July she will be 34 years old"),
+//            //BirthdayViewModel("Eric's birthday is in 10 months, on the 7th of January Eric will be 30 years old"),
+//            //BirthdayViewModel("Ferran's birthday is in 2 months, on the 14th of May he will be 39 years old"),
+//            //BirthdayViewModel("Marc's birthday is today, he is 20 years old")
--- screens/birthday-list/birthday-list-business-logic/src/commonTest/kotlin/com/zegnus/birthdaylist/businesslogic/presentation/BirthdayListPresenterTest.kt
+++ screens/birthday-list/birthday-list-business-logic/src/commonTest/kotlin/com/zegnus/birthdaylist/businesslogic/presentation/BirthdayListPresenterTest.kt
@@ -4,6 +4,7 @@ import com.soywiz.klock.DateTime
 import com.soywiz.klock.Month
 import com.zegnus.birthdaylist.businesslogic.domain.Birthday
 import com.zegnus.birthdaylist.businesslogic.domain.BirthdayListRepository
+import com.zegnus.birthdaylist.businesslogic.domain.Sex
 import com.zegnus.birthdaylist.businesslogic.usecase.BirthdayListUseCase
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -14,42 +15,26 @@ class BirthdayListPresenterTest {
 
     @Test
     fun testExample() {
-//        val presenter = given(
-//            listOf(
-//                Birthday("Anna", DateTime(1987, Month.March, 1), Sex.Female),
-//                //Birthday("Eric", DateTime(1991, Month.January, 7), Sex.NotSpecified),
-//                //Birthday("Ferran", DateTime(1982, Month.May, 14), Sex.Male),
-//                //Birthday("Marc", DateTime(2000, Month.March, 1), Sex.Male)
-//            )
-//        )
-//        val birthdayList = presenter.birthdaysList()
-//
-//        val expectedBirthdayList = listOf(
-//            BirthdayViewModel("Anna's birthday is today, she will is 34 years old"),
-//            //BirthdayViewModel("Anna's birthday is in 4 months, on the 13th of July she will be 34 years old"),
-//            //BirthdayViewModel("Eric's birthday is in 10 months, on the 7th of January Eric will be 30 years old"),
-//            //BirthdayViewModel("Ferran's birthday is in 2 months, on the 14th of May he will be 39 years old"),
-//            //BirthdayViewModel("Marc's birthday is today, he is 20 years old")
-//        )
-//
-//        assertEquals(expectedBirthdayList, birthdayList)
+        val presenter = given(
+            listOf(
+                Birthday("Anna", DateTime(1987, Month.March, 1), Sex.Female),
+                Birthday("Eric", DateTime(1990, Month.March, 2), Sex.NotSpecified),
+                //Birthday("Ferran", DateTime(1982, Month.May, 14), Sex.Male),
+                //Birthday("Marc", DateTime(2000, Month.March, 1), Sex.Male)
+            )
+        )
+        val birthdayList = presenter.birthdaysList()
 
-        val now = DateTime(2020, Month.December, 6)
-        val birthday = DateTime(2020, Month.December, 5)
+        val expectedBirthdayList = listOf(
+            BirthdayViewModel("Anna's birthday is today, she is 34 years old"),
+            BirthdayViewModel("Eric's birthday is tomorrow, Eric will be 20 years old"),
+            //BirthdayViewModel("Anna's birthday is in 4 months, on the 13th of July she will be 34 years old"),
+            //BirthdayViewModel("Eric's birthday is in 10 months, on the 7th of January Eric will be 30 years old"),
+            //BirthdayViewModel("Ferran's birthday is in 2 months, on the 14th of May he will be 39 years old"),
+            //BirthdayViewModel("Marc's birthday is today, he is 20 years old")
+        )
 
-        var diff = birthday.minus(now)
-        var daysRemaining = diff.days.toInt()
-
-        if (daysRemaining < 0) {
-            val nextYear = DateTime(birthday.yearInt + 1, birthday.month, birthday.dayOfMonth)
-            diff = nextYear.minus(now)
-            daysRemaining = diff.days.toInt()
-        }
-
-        val weeksRemaining = diff.weeks.toInt()
-
-        ///assertEquals(0, daysRemaining)
-        assertEquals(0, weeksRemaining)
+        assertEquals(expectedBirthdayList, birthdayList)
     }
 
     private fun given(birthdayLit: List<Birthday>): BirthdayListPresenter {
