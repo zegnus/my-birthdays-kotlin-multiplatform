ecdb9342574f2238fb56f39f239f29724a01b771
Ferran Garriga Olle <zegnus@gmail.com>
2020-12-19 10:18:47 +0000

    reorder code

--- screens/birthday-list/birthday-list-business-logic/src/commonMain/kotlin/com/zegnus/birthdaylist/businesslogic/presentation/BirthdayViewModel.kt
+++ screens/birthday-list/birthday-list-business-logic/src/commonMain/kotlin/com/zegnus/birthdaylist/businesslogic/presentation/BirthdayViewModel.kt
@@ -5,49 +5,49 @@ import com.soywiz.klock.TimeSpan
 import com.soywiz.klock.Year
 import com.zegnus.birthdaylist.businesslogic.domain.Birthday
 import com.zegnus.birthdaylist.businesslogic.domain.Sex
+import com.zegnus.birthdaylist.businesslogic.presentation.WhenIsTheBirthdayType.*
 
 data class BirthdayViewModel(val text: String)
 
 internal fun Birthday.toViewModel(time: Time): BirthdayViewModel {
-    val whenIsTheBirthdayType = whenIsTheBirthdayType(date, time.now())
+    val whenIsTheBirthdayType = whenIsTheBirthdayType(time.now())
     val yearsOld: Int = time.now().year - date.year + 1
 
     val text = when (whenIsTheBirthdayType) {
-        WhenIsTheBirthdayType.Today -> "$name's birthday is today, ${personReference()} is $yearsOld years old"
-        WhenIsTheBirthdayType.Tomorrow -> "$name's birthday is tomorrow, ${personReference()} will be $yearsOld years old"
-        WhenIsTheBirthdayType.NextDay -> "$name's birthday is next ${date.dayOfWeek} on the ${date.dayOfMonth}${date.dayOfMonth.toOrdinal()} of ${date.month}, ${personReference()} will be $yearsOld years old"
-        WhenIsTheBirthdayType.ThisMonth -> "$name's birthday is this month on the ${date.dayOfMonth}${date.dayOfMonth.toOrdinal()} of ${date.month}, ${personReference()} will be $yearsOld years old"
-        WhenIsTheBirthdayType.NextMonth -> "$name's birthday is next month on the ${date.dayOfMonth}${date.dayOfMonth.toOrdinal()} of ${date.month}, ${personReference()} will be $yearsOld years old"
-        WhenIsTheBirthdayType.MoreThanAMonth -> "$name's birthday is in ${monthsLeft(date, time.now())} months on the ${date.dayOfMonth}${date.dayOfMonth.toOrdinal()} of ${date.month}, ${personReference()} will be $yearsOld years old"
+        Today -> "$name's birthday is today, ${personReference()} is $yearsOld years old"
+        Tomorrow -> "$name's birthday is tomorrow, ${personWillBeXYearsOld(yearsOld)}"
+        NextDay -> "$name's birthday is next ${date.dayOfWeek} ${birthdayDate(yearsOld)}"
+        ThisMonth -> "$name's birthday is this month ${birthdayDate(yearsOld)}"
+        NextMonth -> "$name's birthday is next month ${birthdayDate(yearsOld)}"
+        MoreThanAMonth -> "$name's birthday is in ${monthsLeft(time.now())} months ${birthdayDate(yearsOld)}"
     }
     return BirthdayViewModel(text)
 }
 
-private fun monthsLeft(birthdayDate: DateTime, today: DateTime): Int =
-    if (birthdayDate.month > today.month) {
-        birthdayDate.month - today.month
-    } else {
-        12 - today.month1 + birthdayDate.month0
+private fun Birthday.whenIsTheBirthdayType(now: DateTime): WhenIsTheBirthdayType =
+    timeDifference(now, date).let {
+        when {
+            it.days.toInt() == 0 -> Today
+            it.days.toInt() == 1 -> Tomorrow
+            it.days.toInt() <= 7 -> NextDay
+            date.month0 - now.month0 == 0 -> ThisMonth
+            date.month0 - now.month0 == 1 -> NextMonth
+            else -> MoreThanAMonth
+        }
     }
 
-private fun whenIsTheBirthdayType(birthday: DateTime, now: DateTime): WhenIsTheBirthdayType {
-    val timeDifference = timeDifference(now, birthday)
-    return when {
-        timeDifference.days.toInt() == 0 -> WhenIsTheBirthdayType.Today
-        timeDifference.days.toInt() == 1 -> WhenIsTheBirthdayType.Tomorrow
-        timeDifference.days.toInt() <= 7 -> WhenIsTheBirthdayType.NextDay
-        birthday.month0 - now.month0 == 0 -> WhenIsTheBirthdayType.ThisMonth
-        birthday.month0 - now.month0 == 1 -> WhenIsTheBirthdayType.NextMonth
-        else -> WhenIsTheBirthdayType.MoreThanAMonth
+private fun timeDifference(now: DateTime, birthday: DateTime): TimeSpan {
+    val birthdayThisYear = birthday.copyDayOfMonth(year = now.year)
+
+    val diff = birthdayThisYear.minus(now)
+
+    return if (diff.days < 0) {
+        val nextYear = birthdayThisYear.copyDayOfMonth(year = now.year + 1)
+        nextYear.minus(now)
+    } else {
+        diff
     }
 }
-
-private fun Birthday.personReference(): String =
-    when (sex) {
-        Sex.Male -> "he"
-        Sex.Female -> "she"
-        Sex.NotSpecified -> name
-    }
 
 private enum class WhenIsTheBirthdayType {
     Today,
@@ -57,6 +57,20 @@ private enum class WhenIsTheBirthdayType {
     NextMonth,
     MoreThanAMonth,
 }
+
+private fun Birthday.personReference(): String =
+    when (sex) {
+        Sex.Male -> "he"
+        Sex.Female -> "she"
+        Sex.NotSpecified -> name
+    }
+
+private fun Birthday.personWillBeXYearsOld(yearsOld: Int) =
+    "${personReference()} will be $yearsOld years old"
+
+private fun Birthday.birthdayDate(yearsOld: Int) =
+    "on the ${date.dayOfMonth}${date.dayOfMonth.toOrdinal()} of ${date.month}, " +
+            personWillBeXYearsOld(yearsOld)
 
 private fun Int.toOrdinal(): String {
     val hunRem = this % 100
@@ -71,15 +85,9 @@ private fun Int.toOrdinal(): String {
     }
 }
 
-private fun timeDifference(now: DateTime, birthday: DateTime): TimeSpan {
-    val birthdayThisYear = birthday.copyDayOfMonth(year = now.year)
-
-    val diff = birthdayThisYear.minus(now)
-
-    return if (diff.days < 0) {
-        val nextYear = birthdayThisYear.copyDayOfMonth(year = now.year + 1)
-        nextYear.minus(now)
+private fun Birthday.monthsLeft(today: DateTime): Int =
+    if (date.month > today.month) {
+        date.month - today.month
     } else {
-        diff
+        12 - today.month1 + date.month0
     }
-}
